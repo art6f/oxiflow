685f7e4e6a7287896f3e51247104ddec91b325d7
Artem O. <53518804+art6f@users.noreply.github.com>
2026-06-29 11:29:11 +0200

    Misc/tests stats (#23)
    * moved test into the separate folder

--- tests/express-test-server/app/src/index.ts
+++ tests/express-test-server/app/src/index.ts
@@ -3,15 +3,20 @@ import RouterDelay from './routes/delay'
 import RouterRandom from './routes/random'
 import RouterCode from './routes/code'
 import { checkTerminationParam } from './common'
+import { statsMiddleware } from './logging/statsMiddleware'
 
 const app = express();
 
+app.use(statsMiddleware)
+
 app.use(checkTerminationParam);
 
-app.get('/', (req, res) => res.send("It works!"));
+app.get('/', (req, res) => res.send("Express test server.\nUp and running"));
 
 app.use('/delay', RouterDelay);
 app.use('/random', RouterRandom);
 app.use('/code', RouterCode);
 
+
 app.listen(8083, () => "Express Test Server has started");
+
